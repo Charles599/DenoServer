f133133df0b87c448d7bc41ef186eec1e74e5850
charlesdev <ekdekd0909@naver.com>
2020-10-13 15:39:54 +0900

    shorten

--- server.ts
+++ server.ts
@@ -3,8 +3,7 @@ import router from './routes.ts';
 
 const app: Application = new Application();
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(router.routes(), router.allowedMethods());
 
 console.log(`App is Listening on port 8000!`);
 await app.listen({port: 8000});
