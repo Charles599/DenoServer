e2efd01738c772d4db831776053e2aa39da7a799
charlesdev <ekdekd0909@naver.com>
2020-11-27 23:43:11 +0900

    Fix code style

--- server.ts
+++ server.ts
@@ -6,4 +6,4 @@ const app: Application = new Application();
 app.use(router.routes(), router.allowedMethods());
 
 console.log(`App is Listening on port 8000!`);
-await app.listen({port: 8000});
+await app.listen({ port: 8000 });
